5a72e9bf97ea23c4e708e3e82399b0236de2491d
Voa <voalir@yandex.ru>
2023-03-11 03:52:36 +0700

    fix some problems

--- ewm-service/src/main/java/ru/practicum/controller/publicity/CompilationPublicController.java
+++ ewm-service/src/main/java/ru/practicum/controller/publicity/CompilationPublicController.java
@@ -1,10 +1,7 @@
 package ru.practicum.controller.publicity;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.practicum.dto.CompilationDto;
 import ru.practicum.service.CompilationService;
 
@@ -25,7 +22,7 @@ public class CompilationPublicController {
     }
 
     @GetMapping("/{compId}")
-    CompilationDto getCompilationById(@RequestParam Long compId) {
+    CompilationDto getCompilationById(@PathVariable Long compId) {
         return compilationService.getCompilationById(compId);
     }
 }
